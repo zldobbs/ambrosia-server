d8d3bb88124f055fe099c6a2736433991e6fc644
Zach Dobbs <dobbszach@gmail.com>
2024-03-28 23:32:10 -0500

    Get heartbeat working

--- ambrosia.go
+++ ambrosia.go
@@ -10,11 +10,14 @@ import (
 
 // Handle requests to root index
 func indexHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: Debug middleware to log when routes are hit
+	fmt.Println("Index hit")
 	fmt.Fprintf(w, "Welcome to the server!")
 }
 
 // Respond to heartbeat requests to indicate server is reachable and up as expected
 func heartbeatHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Heartbeat hit")
 	fmt.Fprintf(w, "OK")
 }
 
